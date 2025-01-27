68fe06d809ed8ff28a2653e661c89f39973e2ff3
MReyes1241 <Manuelreyes1241@Outlook.com>
2025-01-27 00:17:57 -0500

    Feat: Implement content wrapper for pages
    - Added content wrapper component to standardize page layout.
    - Updated code base to support Mantine breakpoints
    with custom
    variables.
    - Locked dependency updates related to
     PostCSS plugins.

---
+++ src/assets/ContentWrapper.tsx
@@ -0,0 +1,22 @@
+import { Stack } from '@mantine/core'
+import React from 'react'
+import styles from './ContentWrapper.module.css'
+
+type ContentPageProps = {
+  gap?: number
+  children: React.ReactNode
+}
+
+/**
+ * Page wrapper for content pages across the Patina Website to keep a consistent
+ * style.
+ */
+export function ContentPage({ gap,children }: ContentPageProps) {
+  return (
+    <div className={styles.page}>
+      <Stack align={'center'} gap={gap || 0}>
+        {children}
+      </Stack>
+    </div>
+  )
+}
